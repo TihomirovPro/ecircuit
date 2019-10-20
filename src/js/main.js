7f30e5baa3c5eb6a8ac2fc0d2087f3e827bd0dfe
tihomirov.pro <tihomirov.pro@gmail.com>
2019-10-20 22:49:17 +0300

    Added js function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import './utils/fixForEach'
+import { header } from './modules/header'
 
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', function () {
+    let scroll = this.scrollY
+    header(scroll)
+  })
+})
